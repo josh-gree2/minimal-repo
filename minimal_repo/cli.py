1e77def0ea85a28f19440b6ba0c86334d00aab03
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2021-03-15 15:27:18 +0000

    ..

--- minimal_repo/cli.py
+++ minimal_repo/cli.py
@@ -3,7 +3,6 @@ import logging
 import os
 import json
 
-from prefect.engine.results import LocalResult
 from prefect.run_configs import DockerRun
 
 from minimal_repo import all_flows
@@ -29,7 +28,6 @@ stream = client.build(path=".", tag=f"{IMAGE_NAME}:{LOCAL_CONTAINER_TAG}")
 log_stdout_stream(stream)
 
 for flow_name, flow in all_flows.items():
-    flow.result = LocalResult(dir="~/results")
     flow.run_config = DockerRun(
         image=f"{IMAGE_NAME}:{LOCAL_CONTAINER_TAG}",
         env={"PREFECT__FLOWS__CHECKPOINTING": "true"},
--- minimal_repo/flow_1/flow.py
+++ minimal_repo/flow_1/flow.py
@@ -4,6 +4,7 @@ A sample flow def
 from prefect import Flow
 from prefect.storage import Module
 from prefect.executors import LocalDaskExecutor
+from prefect.engine.results import LocalResult
 
 from minimal_repo.flow_1.tasks import foo
 
@@ -15,3 +16,4 @@ with Flow("test_flow") as flow:
 
 flow.storage = Module(__name__)
 flow.executor = LocalDaskExecutor()
+flow.result = LocalResult(dir="~/results")
